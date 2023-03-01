71094516c691ed64affd64bb483e98a0231d3162
dertlimehmet <mehmettdertli@outlook.com>
2023-03-01 14:02:55 +0300

    OgledenSonra

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserModule } from './user/user.module';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UserModule //eager loading
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
