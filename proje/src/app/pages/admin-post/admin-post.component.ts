6515e847bdcd8329249bc0fad50ce7bbe9db8cb9
dertlimehmet <mehmettdertli@outlook.com>
2023-03-07 21:25:10 +0300

    07.03

--- proje/src/app/pages/admin-post/admin-post.component.ts
+++ proje/src/app/pages/admin-post/admin-post.component.ts
@@ -9,11 +9,16 @@ import { VisitorLogService } from '../../visitor-log.service';
 })
 export class AdminPostComponent {
   @Input() postList: ReadonlyArray<Posts> = [];
+  @Output() detailShowClick = new EventEmitter<number>();
 
+  @Input() selectedId: number | undefined;
 
   constructor(private myService: VisitorLogService) {
     this.myService.getPost().subscribe((x) => (this.postList = x));
   }
 
-
+  showDetail(id: number) {
+    this.selectedId = id;
+    this.detailShowClick.emit(this.selectedId);
+  }
 }
--- proje/src/app/pages/visitor/visitor.component.ts
+++ proje/src/app/pages/visitor/visitor.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { Visitor } from '../../visitor';
 import { Publish } from '../../publish';
 import { ContentType } from '../../content-type';
+import { EmailValidator } from '../../email-validator';
 
 @Component({
   selector: 'app-visitor',
@@ -15,12 +16,12 @@ export class VisitorComponent {
   newVisitor: Visitor | undefined = undefined;
 
   public visitorForm = this.formBuilder.group({
-    name: ['',[Validators.required]],
-    email: [''],
-    birthDate: [''],
-    content: [''],
-    contentType: [''],
-    publish: ['1'],
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required, EmailValidator()]],
+    birthDate: ['', [Validators.required]],
+    content: ['', [Validators.required,Validators.minLength(50)]],
+    contentType: ['', [Validators.required]],
+    publish: [''],
   });
 
   publishList: Publish[] = [
@@ -44,6 +45,8 @@ export class VisitorComponent {
     if (!(control.invalid && (control.dirty || control.touched))) return false;
 
     if (control.errors?.['required']) return true;
+    if (control.errors?.['emailFormat']) return true;
+    if(control.errors?.['minlength']) return true;
 
     return false;
   }
